0848e79fd04153a69ff5dccca8aa8b6cc5bd4282
SHIO777 <shiotatsu1015@gmail.com>
2023-04-18 12:40:44 +0900

    add: sample movie

---
+++ sample/plotSettings.m
@@ -0,0 +1,20 @@
+% line width
+set(0, 'DefaultLineLineWidth', 0.8) % default 0.5pt
+set(0, 'DefaultAxesLineWidth', 0.8)
+set(0, 'DefaultTextLineWidth', 0.8)
+
+% font size
+set(0, 'DefaultTextFontSize', 15)
+set(0, 'DefaultAxesFontSize', 15)
+
+% font name
+set(0, 'DefaultTextFontName', 'Times New Roman')
+set(0, 'DefaultAxesFontName', 'Times New Roman')
+set(0, 'DefaultTextInterpreter', 'Latex')
+set(0, 'DefaultLegendInterpreter', 'Latex')
+
+% figure color
+% set(0, 'DefaultFigureWindowStyle', 'docked');
+set(gcf, 'Color', 'none');
+set(gca, 'Color', 'none');
+set(gcf, 'InvertHardCopy', 'off');
